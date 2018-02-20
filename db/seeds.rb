ed81710eef3ecd97a7eafc458613be71bd10d826
isabelle pontoizeau <isa.pontoizeau@gmail.com>
2018-02-20 17:59:47 +0100

    squelette front

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,45 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+i = 0
+
+puts 'Cleaning database...'
+User.destroy_all
+Space.destroy_all
+
+puts 'Creating spaces...'
+
+
+users_attributes = []
+10.times do
+
+  user = {
+  last_name:         Faker::Name.last_name,
+  first_name:        Faker::Name.first_name,
+  phone_number:      Faker::PhoneNumber.phone_number,
+  email:             Faker::Internet.email,
+  password:          Faker::Crypto.md5
+  }
+  users_attributes << user
+end
+
+User.create!(users_attributes)
+
+
+spaces_attributes = []
+10.times do
+
+  space = {
+    name:         Faker::BossaNova.artist,
+    address:      Faker::Address.street_address,
+    user_id:      User.find(users_attributes[i])
+
+  }
+  i += 1
+  spaces_attributes << space
+end
+Space.create!(spaces_attributes)
+puts 'Finished!'
+
+
