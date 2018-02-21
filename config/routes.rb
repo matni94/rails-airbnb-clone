3e4d87ef09a7e4c1071853000dafb73e42028e39
mathieu nicolas <matni9475@gmail.com>
2018-02-21 16:27:58 +0100

    front dev

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ resources :spaces do
 end
 
   devise_for :users
-  root to: 'pages#home'
+  root to: 'spaces#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
