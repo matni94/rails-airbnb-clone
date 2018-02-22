8f7c0044d8636bc413f288a2582794f835dc367c
isabelle pontoizeau <isa.pontoizeau@gmail.com>
2018-02-22 16:20:37 +0100

    last changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,15 +10,8 @@ class UsersController < ApplicationController
 
   def dashboard
     @user = current_user
-    # @user.bookings = Booking.where(user_id: current_user)
-    # @user.spaces = Space.where(user_id: current_user)
-    # @user.spaces.each do |space|
-    #   space.bookings = Booking.where(space_id: space.id)
-    # end
-
     @bookings_visitor = current_user.bookings
     @bookings_owner = current_user.owner_bookings
   end
-
 
 end
